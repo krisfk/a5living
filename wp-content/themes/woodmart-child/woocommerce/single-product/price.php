07ab05f5fe16bca8933494e4f90081a2cdb0c0d5
kaiyu li <krisfk@gmail.com>
2021-12-16 03:36:28 +0800

    .

--- wp-content/themes/woodmart-child/woocommerce/single-product/price.php
+++ wp-content/themes/woodmart-child/woocommerce/single-product/price.php
@@ -26,7 +26,8 @@ global $product;
 <p class="<?php echo esc_attr( apply_filters( 'woocommerce_product_price_class', 'price' ) ); ?>">
     <?php 
     
-   echo $product->get_price_html(); ?>
+//    echo $product->get_price_html();
+    ?>
 
     <?php
                   //   $product_type = $product->get_type();
